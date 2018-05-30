4cdb164e192c51577a8d0e0d20683208c0e44c47
Jason <yhorg@hotmail.com>
2018-05-30 23:29:08 +0800

    Update usecase.

---
+++ Emscripten/em_js/em_js.cc
@@ -0,0 +1,29 @@
+#include <emscripten.h>
+#include <iostream>
+
+using namespace std;
+
+EM_JS(int, add, (int x, int y), {
+	console.log(x, y);
+  return x + y;
+});
+
+int main(int argc, char** argv) {
+  cout << add(1, 2) << endl;
+  return 0;
+}
+
+/*
+
+#define EM_JS(ret, name, params, ...)          \ 
+   _EM_JS_CPP_BEGIN                             \ 
+   ret name params;                             \ 
+   __attribute__((used, visibility("default"))) \ 
+   const char* __em_js__##name() {              \ 
+     return #params "<::>" #__VA_ARGS__;        \ 
+   }                                            \ 
+   _EM_JS_CPP_END 
+  
+ #endif // __em_js_h__ 
+
+*/
